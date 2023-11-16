5a1a951ea864e55cbea72773e236f14284240e81
Chris Najman <christopher.najman@gmail.com>
2023-11-16 14:20:40 +0000

    Top level only - remove linksto sub folder

--- js/index.js
+++ js/index.js
@@ -1,31 +1,3 @@
 import setCurrentPageMenuItem from "./set-current-page-menu-item.js"
 
 document.body.onload = setCurrentPageMenuItem()
-
-// console.log("Location href:")
-// console.log(location.href)
-
-// console.log("Location pathname:")
-// console.log(location.pathname)
-
-// console.log("Location protocol")
-// console.log(location.protocol)
-
-// console.log("Location hostname")
-// console.log(location.hostname)
-
-// const locationProtocol = location.protocol
-// const locationHostname = location.hostname
-// const locationPathname = location.pathname
-
-// const locationString = `${locationProtocol}//${locationHostname}${locationPathname}`
-
-// console.log(locationString)
-
-// console.log(location.pathname.split("/").pop())
-
-const pop = location.pathname.split("/").pop()
-
-if (pop === "") {
-  console.log("pop!")
-}
