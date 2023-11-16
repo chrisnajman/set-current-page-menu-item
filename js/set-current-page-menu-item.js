0bee2bca43b27ffeb1185d0232895ad69e844aee
Chris Najman <christopher.najman@gmail.com>
2023-11-16 14:34:36 +0000

    Replaced link.pathname with link.href

--- js/set-current-page-menu-item.js
+++ js/set-current-page-menu-item.js
@@ -2,7 +2,7 @@ export default function setCurrentPageMenuItem() {
   const primaryNavLinks = document.querySelectorAll(".primary-navigation a")
   primaryNavLinks.forEach((link) => {
     if (
-      link.pathname.split("/").pop() === "index.html" &&
+      link.href.split("/").pop() === "index.html" &&
       location.href.split("/").pop() === ""
     ) {
       link.setAttribute("aria-current", "page")
